80e6059351d48d28ec9a8d148627a2e494027f98
Exequiel-2023 <myeboutique2015@gmail.com>
2024-04-11 04:20:59 -0300

    estoy seguro de este commit 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numero1 = 0; 
+int numero2 = 0;
+int suma = 0;
+
+Console.WriteLine("ingrese el primer numero");
+numero1=int.Parse(Console.ReadLine());
+
+Console.WriteLine("ingrese el segundo numero");
+numero2 = int.Parse(Console.ReadLine());
+
+suma = numero1 + numero2;
+Console.WriteLine("el resultado de la suma es " +suma);
+Console.ReadKey();
